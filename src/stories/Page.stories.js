92e223f5ef9f1b693a9c093706e2e79304c2a009
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2024-04-01 10:20:11 +0200

    initial commit

---
+++ src/stories/Page.stories.js
@@ -0,0 +1,36 @@
+import { within, userEvent, expect } from '@storybook/test';
+
+import { Page } from './Page';
+
+export default {
+  title: 'Przyklad/Page',
+  component: Page,
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export const LoggedOut = {
+  play: async ({ canvasElement} ) => {
+    const canvas = within(canvasElement);
+    const signUpButton = canvas.getByRole('button', {name: /Sign up/i})
+    await expect(signUpButton).toBeInTheDocument();
+    await userEvent.click(signUpButton)
+  },
+};
+
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const LoggedIn = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const loginButton = canvas.getByRole('button', { name: /Log in/i });
+    await expect(loginButton).toBeInTheDocument();
+    await userEvent.click(loginButton);
+    await expect(loginButton).not.toBeInTheDocument();
+
+    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
+    await expect(logoutButton).toBeInTheDocument();
+  },
+};
